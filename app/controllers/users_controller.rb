e633b1922ad7d6f93cd9d19879d76031165ad279
zaplata <zaplatamaxime@gmail.com>
2015-07-06 11:50:59 +0200

    comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,32 +1,30 @@
 class UsersController < ApplicationController
 
-	def index
-		@user = User.all
+	def index 
+		@user = User.all # Take all user from database 
 	end
 
 	def new
-		@user = User.new
+		@user = User.new 
 	end
 
 	def show 
-		@user = User.find(session[:user_id])
-
+		@user = User.find(session[:user_id]) #take one user from database by session ID
 	end
 
 	def create
-		@user = User.new(user_params)
-		if @user.save
-			flash[:success] ="okokokokok"
-			session[:user_id] = @user.id
-			redirect_to '/login'  
+		@user = User.new(user_params) # create a new user with fillable database field
+		if @user.save # if user was save in DB
+			flash[:success] ="User has been registered" #show a success message
+			session[:user_id] = @user.id #create a new session with ID user 
+			redirect_to '/'  #redirect to root_path 
 		else
-			render 'new'
+			render 'new' #show 'new' view
 		end
 	end
 
 	private
-		def user_params
+		def user_params #prepare database field to be filled
 			params.require(:user).permit(:name,:firstname,:adress,:phone,:specialty,:password)
-		end
-		
+		end	
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ActiveRecord::Base
 
-	has_secure_password
+	has_secure_password #create password_digest
+
+	#create rules for user form 
+	validates :name, :presence => true
+	validates :firstname, :presence => true
+	validates :adress, :presence => true
+	validates :phone, :presence => true
+	validates :password, :presence => true
+	validates :specilaty, :presence => true 
 
 end
